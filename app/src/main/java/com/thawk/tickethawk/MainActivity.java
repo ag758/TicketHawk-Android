56ec0f5168e47cabb19e664c899d2ce2cb3ce897
Austin Gao <agao98@gmail.com>
2019-10-11 00:12:40 -0400

    Initial Commit

--- app/src/main/java/com/thawk/tickethawk/MainActivity.java
+++ app/src/main/java/com/thawk/tickethawk/MainActivity.java
@@ -2,14 +2,18 @@ package com.thawk.tickethawk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_main);
-        System.out.println("hello");
     }
 }
