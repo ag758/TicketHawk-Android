4c67c8508ce0a74b9343a720616c6d2d2c7b43c3
Austin Gao <agao98@gmail.com>
2019-10-15 17:39:43 -0400

    Ticket Number Controller

--- app/src/main/java/com/thawk/tickethawk/StripeActivity.java
+++ app/src/main/java/com/thawk/tickethawk/StripeActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,9 +23,13 @@ import com.stripe.android.model.Token;
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONObject;
 
 import java.io.BufferedWriter;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
@@ -44,6 +49,8 @@ public class StripeActivity extends Activity {
     Card card;
     Token tok;
 
+    Button submitButton;
+
     public String pKey;
 
     StripeActivity sA;
@@ -57,9 +64,13 @@ public class StripeActivity extends Activity {
         name = "TicketHawk";
 
         stripe = new Stripe(this, getResources().getString(R.string.publishableKey));
+        submitButton = (Button)findViewById(R.id.submitButton);
     }
 
     public void submitCard(View v){
+
+        submitButton.setEnabled(false);
+
         TextView cardNumberField = (TextView) findViewById(R.id.cardNumber);
         TextView monthField = (TextView) findViewById(R.id.month);
         TextView yearField = (TextView) findViewById(R.id.year);
@@ -73,20 +84,19 @@ public class StripeActivity extends Activity {
         ).build();
 
 
-
         stripe.createToken(
                 card,
                 new ApiResultCallback<Token>() {
                     public void onSuccess(@NonNull Token token) {
                         // send token ID to your server, you'll create a charge next
-
+                        Toast.makeText(getApplicationContext(), "Token created: " + token.getId(), Toast.LENGTH_LONG).show();
                         tok = token;
                         new StripeCharge(token.getId()).execute();
                     }
 
                     @Override
                     public void onError(@NotNull Exception e) {
-
+                        Log.d("Stripe", e.getLocalizedMessage());
                     }
                 }
         );
@@ -94,6 +104,8 @@ public class StripeActivity extends Activity {
 
     public class StripeCharge extends AsyncTask<String, Void, String> {
         String token;
+        String data = "";
+        HttpURLConnection conn = null;
 
         public StripeCharge(String token) {
             this.token = token;
@@ -104,68 +116,95 @@ public class StripeActivity extends Activity {
             new Thread() {
                 @Override
                 public void run() {
-                    postData(name,token,"" + amount);
+                    postData(name, token, "" + amount);
                 }
             }.start();
-            return "Done";
+            return data;
         }
 
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            Log.e("Result",s);
+            Log.e("Result", s);
         }
 
-        public void postData(String description, String token,String amount) {
+        public void postData(String description, String token, String amount) {
             // Create a new HttpClient and Post Header
             try {
                 URL url = new URL(getResources().getString(R.string.baseURLString));
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setReadTimeout(10000);
-                conn.setConnectTimeout(15000);
+                conn = (HttpURLConnection) url.openConnection();
                 conn.setRequestMethod("POST");
                 conn.setDoInput(true);
                 conn.setDoOutput(true);
+
+                Log.e("TAG", "hello");
 
                 List<NameValuePair> params = new ArrayList<NameValuePair>();
                 params.add(new BasicNameValuePair("method", "charge"));
                 params.add(new BasicNameValuePair("description", description));
                 params.add(new BasicNameValuePair("source", token));
                 params.add(new BasicNameValuePair("amount", amount));
+                params.add(new BasicNameValuePair("currency", "usd"));
 
-                OutputStream os = null;
+                DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+                wr.writeBytes(getQuery(params));
+                wr.flush();
+                wr.close();
 
-                os = conn.getOutputStream();
-                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
-                writer.write(getQuery(params));
-                writer.flush();
-                writer.close();
-                os.close();
+                InputStream in = conn.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(in);
 
-            } catch (IOException e) {
+                int inputStreamData = inputStreamReader.read();
+                while (inputStreamData != -1) {
+                    char current = (char) inputStreamData;
+                    inputStreamData = inputStreamReader.read();
+                    Log.e("TAG", "hello2" + data);
+                    data += current;
+                }
+            } catch (Exception e) {
                 e.printStackTrace();
+                Log.e("TAG", "hello3");
+            } finally {
+
+                if (data.contains("Success")){
+                    //Success
+                } else {
+
+                }
+                Log.e("TAG", "hello4");
+                if (conn != null){
+                    conn.disconnect();
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        submitButton.setEnabled(true);
+                    }
+                });
+
+
             }
         }
+    }
 
-        private String getQuery(List<NameValuePair> params) throws UnsupportedEncodingException
+    private String getQuery(List<NameValuePair> params) throws UnsupportedEncodingException
+    {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+
+        for (NameValuePair pair : params)
         {
-            StringBuilder result = new StringBuilder();
-            boolean first = true;
+            if (first)
+                first = false;
+            else
+                result.append("&");
 
-            for (NameValuePair pair : params)
-            {
-                if (first)
-                    first = false;
-                else
-                    result.append("&");
-
-                result.append(URLEncoder.encode(pair.getName(), "UTF-8"));
-                result.append("=");
-                result.append(URLEncoder.encode(pair.getValue(), "UTF-8"));
-            }
-
-            return result.toString();
+            result.append(URLEncoder.encode(pair.getName(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(pair.getValue(), "UTF-8"));
         }
+
+        return result.toString();
     }
 
 
